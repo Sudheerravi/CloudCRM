bdd38064616678821d9f48072cc372fd0855f7d2
Sudheerravi <sudheerravi@hotmail.com>
2018-05-13 11:47:20 -0500

    Add files via upload

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrmMvc.Models;
+
+namespace CrmMvc.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactDbEntities db = new ContactDbEntities();
+
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View(db.contactDbs.ToList());
+        }
+
+        
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Search")]
+        public ActionResult SearchConfirmed(string DomainURL)
+        {
+            List<contactDb> myList = db.contactDbs.ToList();
+            contactDb results = myList.SingleOrDefault(s => s.DomainURL == DomainURL);
+            
+            //db.contactDbs.Remove(contactdb);
+            //db.SaveChanges();
+            return RedirectToAction("Details", "Contact", new { ID = results.ID });
+        }
+        //
+        // GET: /Contact/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            contactDb contactdb = db.contactDbs.Find(id);
+            if (contactdb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactdb);
+        }
+
+        //
+        // GET: /Contact/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Contact/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(contactDb contactdb)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                contactdb.ID = db.contactDbs.ToList()[db.contactDbs.ToList().Count-1].ID + 1;
+                
+                db.contactDbs.Add(contactdb);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactdb);
+        }
+
+        //
+        // GET: /Contact/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            contactDb contactdb = db.contactDbs.Find(id);
+            if (contactdb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactdb);
+        }
+
+        //
+        // POST: /Contact/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(contactDb contactdb)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactdb).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactdb);
+        }
+
+        //
+        // GET: /Contact/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            contactDb contactdb = db.contactDbs.Find(id);
+            if (contactdb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactdb);
+        }
+
+        //
+        // POST: /Contact/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            contactDb contactdb = db.contactDbs.Find(id);
+            db.contactDbs.Remove(contactdb);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/Default1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace CrmMvc.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        public ActionResult Redirects()
+        {
+            //return new RedirectResult("http://www.wellsfargo.com");
+            return GetHtmlPage(".\\Sample.html");
+
+        }
+        [ChildActionOnly]
+        public ActionResult GetHtmlPage(string path)
+        {
+            return new FilePathResult(path, "text/html");
+        }
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            foreach (string upload in Request.Files)
+            {
+                if (Request.Files[upload].FileName != "")
+                {
+                    string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/uploads/";
+                    string filename = Path.GetFileName(Request.Files[upload].FileName);
+                    Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                }
+            } 
+          // return View("Upload");
+            return View();
+        }
+       
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id)
+        {
+           // Response.Redirect("/app_data/uploads/microsoft.pdf");
+            Response.Redirect("https://sudhe29000.wixsite.com/mysite");
+            return View();
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
